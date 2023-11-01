197170f78c7b6945a0f65d897c77b321900756b3
Vaibhav Shukla <vaibhavshukla0505@gmail.com>
2023-11-01 23:31:29 +0530

    Saving data to the mongoose database

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const express = require("express")//express module imported
 const path=require('path');
 const app=express();//created app
+const bodyparser=require("body-parser")
 const port =3000;
+
+//
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/danceContact');
+}
+//Define mongoose schema
+const contactSchema = new mongoose.Schema({
+    name: String,
+    Phone: String,
+    Email: String,
+    desc: String
+});
+
+const contact = mongoose.model('contact', contactSchema);
 
 //EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static'))//serving  static file---ie file can be used by anyone anytime without permission or login
@@ -20,6 +39,17 @@ app.get('/', (req, res) => {
 app.get('/contact', (req, res) => {
     const params = {}
     res.status(200).render('contact.pug', params)
+})
+
+app.post('/contact', (req, res) =>{
+    var myData= new contact(req.body);
+    myData.save().then(()=>{
+        res.send("this data has been saved to the database")
+    })
+    .catch(()=>{
+        res.status(400).send("data has not been saved to the databade")
+    })
+   //res.status(200).render('contact.pug')
 })
 
 //START THE SERVER
